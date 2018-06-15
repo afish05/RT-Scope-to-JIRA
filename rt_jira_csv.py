4ce6bab614a6461368ac07aef45c1658097c8a12
afish05 <adamfisher86@gmail.com>
2018-06-15 16:01:54 -0500

    Add files via upload

--- rt_jira_csv.py
+++ rt_jira_csv.py
@@ -2,38 +2,110 @@
 # Author: afisher
 # This script will output the .csv for creating JIRA sub-tasks using the RM scope .csv as input 
 
+import sys
+import os
 import csv
+import time
 
-print('Welcome to the JIRA RT .csv script! \n\nPlease ensure your input is named "scope.csv" and the file is located in the same directory as this script\n')
+cwd = os.getcwd() #Get the current directory, script will run on input from the same.
+verify = 'N'
 
-hub = input('Enter DC4 hub company name: ')
-doc = input('Enter the EDI doc type: ')
-rt_task = input('Enter the master RT ticket number: ')
-sch_dt = input('Enter the scheduled install date (MM/DD/YYYY): ')
-owner = input('Enter your email address: ')
+print('Welcome to the JIRA RT .csv script! \n\nPlease ensure your input file is located in the same directory as this script\n')
 
-infile = open('scope.csv', 'r') #Name your file 'scope.csv'
-csv_reader = csv.DictReader(infile, dialect='excel')
+def main():
+    
+    hub = input('Enter DC4 hub company name: ')
+    doc = input('Enter the EDI doc type: ')
+    rt_task = input('Enter the master RT ticket number: ')
+    sch_dt = input('Enter the scheduled install date (MM/DD/YYYY): ')
+    owner = input('Enter your email address: ')
+    filename = input('Enter the file name (with file extension) to run: ')
 
-outfile = open('JIRA_Import.csv', 'w', newline='')
-out_clmns = ['Type','Issue ID','Parent ID','Summary','Description','Scheduled','Account Name','RM Coordinator','Project Owner','Summary of Changes','TPID']
-csv_writer = csv.DictWriter(outfile, fieldnames=out_clmns, dialect='excel')
-csv_writer.writeheader()
+    #Exit script function
+    def die():
+        os.system("pause")
+        sys.exit()
+	
+	#Restart script function
+    def restart():
+        print('\nRestarting Script\n')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.')
+        time.sleep(1)
+        print('.\n')
+        main()
 
-for row in csv_reader:
-    co_name = (row['COMPANY_NAME'])
-    tpid = (row['MAX(SP.TPID)'])
-    csv_writer.writerow({'Type':'Sub-task',
-                         'Parent ID':(rt_task),
-                         'Summary':(co_name)+' / '+(hub)+' / '+(doc),
-                         'Description':'See Summary of Changes',
-                         'Scheduled':(sch_dt),
-                         'Account Name':(co_name),
-                         'RM Coordinator':(owner),
-                         'Project Owner':(owner),
-                         'TPID':(tpid) or 'SALESFORCE'})      
+    #Restart script for null inputs
+    if hub == '' or doc == '' or rt_task == '' or sch_dt == '' or owner == '' or filename == '':
+        print('\nInput Parameter missing.\n')
+        restart()
 
-infile.close()
-outfile.close()
+	#User verify correct inputs
+    verify = input('\nIs the above information correct? (Y/N): ')
+    if verify == 'Y' or verify == 'y':
+		
+		#Format the input file name
+        file = (cwd)+'\\'+(filename)
+        
+        #Open and read the input file
+        try:
+            with open((file), 'r') as chkfile:
+                chkfile.read()
 
-print('\nDONE - Script created JIRA_Import.csv. Please check the accuracy of its contents before uploading to JIRA')
+        #Fail for no file found
+        except FileNotFoundError:
+            print('\nERROR - Input file '+(filename)+' not found in '+(cwd)+'\n')
+            die()
+
+        #Fail non-text binary files
+        except UnicodeDecodeError:
+            print('\nERROR - Invalid input file. Please ensure that the file is saved in .csv format\n')
+            die()
+
+        #Fail other stuff
+        except Exception:
+            print('\nAn unexpected error occured\n')
+            die()
+
+        try:
+            #Create the output file and assign column headers
+            with open((cwd)+'\JIRA_Import_'+(rt_task)+'.csv', 'w', newline='') as outfile:
+                out_clmns = ['Type','Issue ID','Parent ID','Summary','Description','Scheduled','Account Name','RM Coordinator','Project Owner','Summary of Changes','TPID']
+                csv_writer = csv.DictWriter(outfile, fieldnames=out_clmns, dialect='excel')
+                csv_writer.writeheader()
+
+                #Read the input file. Skip blank rows
+                with open((file), 'r') as infile:
+                    csv_reader = csv.DictReader((line for line in infile if not line.startswith(',')), dialect='excel')
+
+                    #Build the output csv
+                    for row in csv_reader:
+                        co_name = (row['COMPANY_NAME'])
+                        tpid = (row[str(csv_reader.fieldnames[1])]) #Second column for TPID. Header differs.
+                        csv_writer.writerow({'Type':'Sub-task',
+                                        'Parent ID':(rt_task),
+                                        'Summary':(co_name)+' / '+(hub)+' / '+(doc),
+                                        'Description':'See Summary of Changes',
+                                        'Scheduled':(sch_dt),
+                                        'Account Name':(co_name),
+                                        'RM Coordinator':(owner),
+                                        'Project Owner':(owner),
+                                        'TPID':(tpid) or 'SALESFORCE'})
+
+                    print('\nSUCCESS - Script created '+(outfile.name)+'.\n\nPlease check the accuracy of its contents before uploading to JIRA\n\n')
+                    die()
+		
+		#Report failures and do not create an output
+        except Exception:
+            os.remove(outfile.name)
+            print('\nFAIL - An Error Occured :(\n')
+            die()
+
+    else:
+        restart()
+
+if __name__ == "__main__":
+    main()
+
